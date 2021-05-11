80d773aba0d5a352cace2038d2a123a94b633c77
Igor Leonardo <igor.leonardo@gmail.com>
2021-05-11 13:11:34 -0300

    edit contact

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Contact } from '../shared/models/contact';
 import { ContactService } from '../shared/services/contact.service';
 
@@ -12,11 +13,16 @@ export class ContactsComponent implements OnInit {
   public contacts: Contact[];
 
   constructor(
-    private contactService: ContactService
+    private contactService: ContactService,
+    private route: Router
   ) { }
 
   ngOnInit(): void {
     this.contacts = this.contactService.getContacts();
+  }
+
+  editContact(index) {
+    this.route.navigate(['edit-contact/'+index])
   }
 
   deleteContact(index) {
--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Contact } from '../shared/models/contact';
 import { ContactService } from '../shared/services/contact.service';
 
@@ -9,17 +11,33 @@ import { ContactService } from '../shared/services/contact.service';
 })
 export class EditContactComponent implements OnInit {
 
-  private contact: Contact;
+  public contact: Contact;
 
   constructor(
-    private contactService: ContactService
+    private contactService: ContactService,
+    private route: ActivatedRoute
   ) { }
 
-  @Input() index;
+  contactForm = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    phone: new FormControl(null, [Validators.required]),
+    type: new FormControl('pessoal', [Validators.required])
+  }); 
+
+  index = Number(this.route.snapshot.paramMap.get('id'));
 
   ngOnInit(): void {
     let contacts = this.contactService.getContacts();
     this.contact = contacts.splice(this.index, 1);
+
+    console.log(this.contact);
+
+    this.contactForm.patchValue({
+      name: this.contact.name,
+      type: this.contact.type,
+      phone: this.contact.phone
+    });
+
   }
 
 }
